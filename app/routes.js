2100407153997505a6e6d7525735b556293ad881
ptlergo <ptlergo@gmail.com>
2016-05-12 19:51:32 -0400

    created views with ejs engine. new packages installed and saved

--- app/routes.js
+++ app/routes.js
@@ -4,8 +4,32 @@ module.exports=function(app){
 
   app.get('/', function(req, res){
 
-    res.send('hello');
+    res.render('index');
 
+  });
+
+  app.get('/signup', function(req, res){
+
+    res.render('signup', {message: 'victory'});
+
+  });
+
+  //form submission from signup page
+  app.post('/signup', function(req, res){
+
+    const newUser=new User();
+    //save parameters from route into database
+    newUser.local.username=req.body.email;
+    newUser.local.password=req.body.password;
+    console.log(newUser.local.username+" "+newUser.local.password);
+    //save to database
+    newUser.save(function(err){
+      if(err)
+        throw err;
+
+    });
+
+    res.redirect('/');
   });
 
   //get parameters from route
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const app=express();
 const morgan=require('morgan');
+const body_parser=require('body-parser');
+const ejs=require('ejs');
 const cookie_parser=require('cookie-parser');
 const session=require('express-session');
-const port=process.env.PORT||3000;
 const mongoose=require('mongoose');
+const port=process.env.PORT||3000;
 
 //mongo database setup
 const configDB=require('./config/database.js');
@@ -14,10 +16,14 @@ mongoose.connect(configDB.url);
 app.use(morgan('dev'));
 //log client and server transaction. saves cookies(not neccesary)
 app.use(cookie_parser());
+//html form accepts text, arrays, objects, etc.
+app.use(body_parser.urlencoded({extended: true}));
 app.use(session({secret: 'anystringoftext',
                  saveUninitialized: true,
                  resave: true}));
 
+//set template engine for webpages
+app.set('view engine', 'ejs');
 
 require('./app/routes.js')(app);
 
